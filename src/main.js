c7f6c7d0d7532689ea7a8234375a6ee1d05e059c
Sofia <81938344+Sofia0422@users.noreply.github.com>
2022-08-11 17:53:55 -0500

    agrega funcion menutoggle

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-/*Mostrar y ocultar contenido*/
+/*import data from './data/lol/lol.js';
+console.log(data.data.Aatrox.id);*/
+
+//Función Menú toggle//
+
+let menutoggle = document.querySelector(".menutoggle");
+let menutoggleIcon = document.querySelector(".menutoggle i");
+let container = document.getElementById("container");
+
+menutoggle.addEventListener("click" , e=> {
+  container.classList.toggle("show"); 
+
+  if(container.classList.contains("show")) {
+    menutoggleIcon.setAttribute("class" , "fas fa-times");
+  } else{
+      menutoggleIcon.setAttribute("class" , "fas fa-bars");
+    }
+  });
+
+//Mostrar y ocultar contenido//
 let BotonConoceMas = document.getElementById("conoceMas");
 BotonConoceMas.addEventListener("click", mostrarConoceMas);
 
 function mostrarConoceMas() {
-    document.getElementById("page2").style.display = "block";
-    document.getElementById("page1").style.display = "none";
+    document.getElementById("nuestros-programas").style.display = "block";
+    document.getElementById("hero").style.display = "none";
+    document.getElementById("somos-proya").style.display = "none";
   }
-
-console.log(example, data);
