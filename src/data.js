0672f87229ca94365b413833b03e174e7d172a1e
Tatiana Sanabria Pedraza <tatianasanabr@Invitados-MacBook-Pro.local>
2022-08-16 19:38:32 -0500

    Ajuste menu responsive y visualización campeones

--- src/data.js
+++ src/data.js
@@ -1,9 +1,73 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const ordenarCampeones = (listaTodos, term) => {
+  let ordenarLista;
+  if (term === 'az') {
+    ordenarLista = Object.values(listaTodos).sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+  }
+  if (term === 'za') {
+    ordenarLista = Object.values(listaTodos)
+      .sort((a, b) => {
+        if (a.name < b.name) {
+          return 1;
+        }
+        if (a.name > b.name) {
+          return -1;
+        }
+        return 0;
+      });
+  }
+  return ordenarLista;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+
+export const filtrarNombre = (data, texto) => {
+  const longitudcadenaBuscada = texto.length;
+  const filtroCampeones = Object.values(data).filter((campeones) => (
+    texto === campeones.name.toLowerCase().substring(0, longitudcadenaBuscada)
+  ));
+  return filtroCampeones; // retornamos el array de objetos encontrados
+};
+
+export const filtrandoRoles = (data, rolEscogido) => {
+  const newArray = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const arrTags = data[i].tags;
+    for (let j = 0; j < arrTags.length; j += 1) {
+      if (arrTags[j] === rolEscogido) {
+        newArray.push(data[i]);
+      }
+    }
+  }
+  return newArray;
+};
+export const calculoStats = (data, entrada) => {
+  let item = 0;
+  let result = 0;
+  switch (entrada) {
+    case 'hp':
+      item = data.map((campeones) => campeones.stats);
+      result = (item.reduce((sum, value) => (sum + value.hp), 0) / 134).toFixed(2);
+      break;
+    case 'movespeed':
+      item = data.map((campeones) => campeones.stats);
+      result = (item.reduce((sum, value) => (sum + value.movespeed), 0) / 134).toFixed(2);
+      break;
+    case 'attackdamage':
+      item = data.map((campeones) => campeones.stats);
+      result = (item.reduce((sum, value) => (sum + value.attackdamage), 0) / 134).toFixed(2);
+      break;
+    default:
+      break;
+  }
+  return result;
 };
--- src/main.js
+++ src/main.js
@@ -1,29 +1,44 @@
+import lol from './data/lol/lol.js';
+import {
+  filtrarNombre, ordenarCampeones, filtrandoRoles, calculoStats,
+} from './data.js';
 
-/*import data from './data/lol/lol.js';
-console.log(data.data.Aatrox.id);*/
+const listaTodos = lol.data; // en esta variable guardo los objetos de la data
+const arrayCampeones = Object.values(listaTodos);
 
-//Función Menú toggle//
+const lista = document.querySelector('#galeria'); // selecciono la parte donde voy a poner la informacion
+const cantidadCampeones = document.getElementById('cantidad');
 
-let menutoggle = document.querySelector(".menutoggle");
-let menutoggleIcon = document.querySelector(".menutoggle i");
-let container = document.getElementById("container");
-
-menutoggle.addEventListener("click" , e=> {
-  container.classList.toggle("show"); 
-
-  if(container.classList.contains("show")) {
-    menutoggleIcon.setAttribute("class" , "fas fa-times");
-  } else{
-      menutoggleIcon.setAttribute("class" , "fas fa-bars");
-    }
+const resultado = (data) => {
+  // vaciar arreglo para que no se duplique
+  lista.innerHTML = '';
+  error.classList.add('ocultar');
+  // chequear la estructura de la data, si no es un array pasa a Object.values
+  const checkData = Array.isArray(data) ? data : Object.values(data);
+  cantidadCampeones.innerHTML = `Number Of Champions:  ${checkData.length}`;
+  checkData.forEach((campeones) => {
+    const casilla = document.createElement('div');
+    const foto = document.createElement('img');
+    const nombre = document.createElement('p');
+    nombre.className = 'nombreCampeon'; // le pongo un class nombre para editarle los estilos
+    foto.className = 'imagenCampeon';
+    nombre.innerHTML = `${campeones.name}`;
+    foto.src = `${campeones.splash}`;
+    casilla.className = 'casillaCampeon';
+    casilla.id = campeones.name;
+    casilla.appendChild(foto);
+    casilla.appendChild(nombre);
+    lista.appendChild(casilla);
+    // casilla agregar add event ... crearVistaCampeon(identificador);
+    casilla.addEventListener('click', (evt) => {
+      const nombreId = evt.path[1].id;
+      crearVistaCampeon(listaTodos[nombreId]);
+      pantalla3.classList.remove('ocultar');
+      lista.innerHTML = '';
+      pantalla2.classList.remove('mostrar');
+      pantalla2.classList.add('ocultar');
+      error.classList.add('ocultar');
+    });
   });
-
-//Mostrar y ocultar contenido//
-let BotonConoceMas = document.getElementById("conoceMas");
-BotonConoceMas.addEventListener("click", mostrarConoceMas);
-
-function mostrarConoceMas() {
-    document.getElementById("nuestros-programas").style.display = "block";
-    document.getElementById("hero").style.display = "none";
-    document.getElementById("somos-proya").style.display = "none";
-  }
+};
+resultado(listaTodos);
